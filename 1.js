90faa145d74b53697f62a5d4651df0a5ee2d3828
Sanket Priyadarshi <51432182+s-priyadarshi@users.noreply.github.com>
2023-07-18 23:47:19 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -2,12 +2,12 @@ var gr = new GlideRecord('sys_properties');
 gr.addQuery('name', 'glide.entry.first.page.script');
 gr.query();
 if (gr.next()) {
-  gs.info("Record with the name 'glide.entry.first.page.script' already exists. Skipping creation.");
+  gs.info('Record with the name glide.entry.first.page.script already exists. Skipping creation.');
 } else {
   gr = new GlideRecord('sys_properties');
   gr.initialize();
   gr.name = 'glide.entry.first.page.script';
   gr.value = 'new SPEntryPage().getFirstPageURL()';
   if (gr.insert() != null)
-    gs.info("Record with the name 'glide.entry.first.page.script' and the value 'new SPEntryPage().getFirstPageURL()' created successfully.");
+    gs.info('Record with the name glide.entry.first.page.script and the value new SPEntryPage().getFirstPageURL() created successfully.');
 }
